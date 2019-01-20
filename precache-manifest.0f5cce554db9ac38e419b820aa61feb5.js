e9f99bb0e7c37910eda3227b66946c403ca55813
bobbeugels <bob.beugels@gmail.com>
2019-01-20 19:31:07 +0530

    Updates

---
+++ precache-manifest.0f5cce554db9ac38e419b820aa61feb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a282d6d8003d6685b665",
+    "url": "/BBBGLS/static/js/runtime~main.a282d6d8.js"
+  },
+  {
+    "revision": "3bfb0759421c63d04362",
+    "url": "/BBBGLS/static/js/main.3bfb0759.chunk.js"
+  },
+  {
+    "revision": "24a685e0ab5572253d99",
+    "url": "/BBBGLS/static/js/1.24a685e0.chunk.js"
+  },
+  {
+    "revision": "3bfb0759421c63d04362",
+    "url": "/BBBGLS/static/css/main.ebc71122.chunk.css"
+  },
+  {
+    "revision": "975f435c7b932d1e4ec85f6e144560d3",
+    "url": "/BBBGLS/index.html"
+  }
+];
