b110f4b37403e7a16958071a59e5673a941c8056
bobbeugels <bob.beugels@gmail.com>
2019-01-24 19:37:27 +0100

    Updates

---
+++ precache-manifest.3b50c2b1680d4606606e049954b6e946.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aded534f913ae0861588",
+    "url": "/static/js/main.aded534f.chunk.js"
+  },
+  {
+    "revision": "45a76e73940dec5f5fcc",
+    "url": "/static/js/1.45a76e73.chunk.js"
+  },
+  {
+    "revision": "aded534f913ae0861588",
+    "url": "/static/css/main.c867821b.chunk.css"
+  },
+  {
+    "revision": "41326cce06e6a9ac8b84446c7506af71",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/BBBGLS/precache-manifest.678953134fa42c0bcfb0a1afac929d9a.js"
+  "/precache-manifest.3b50c2b1680d4606606e049954b6e946.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/BBBGLS/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
