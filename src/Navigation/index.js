815736b27265aa12d56b4c4659a911e1793e7a8d
bobbeugels <bob.beugels@gmail.com>
2019-02-07 21:47:07 +0100

    Refactored Navigation with hooks

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,45 +1,29 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.scss';
 
-export default class Navigation extends PureComponent {
-  state = {
-    hoveringOnLastItem: false,
-  };
+export default function Navigation() {
+  const [hoveringOnLastItem, hoverHandler] = useState(false);
 
-  hoverHandler = (value) => {
-    this.setState({ hoveringOnLastItem: value });
-  };
-
-  render() {
-    const { hoveringOnLastItem } = this.state;
-
-    return (
-      <div className="Navigation">
-        <div className="logo">
-          BBBGLS
-        </div>
-        <ul>
-          <li>
-            <Link to="/BBBGLS/">About me</Link>
-          </li>
-          <li>
-            <Link to="/BBBGLS/abilities">Abilities</Link>
-          </li>
-          <li>
-            <Link to="/BBBGLS/work">Work</Link>
-          </li>
-          <li className={hoveringOnLastItem ? 'hover' : ''}>
-            <Link to="/BBBGLS/education">Education</Link>
-          </li>
-          <li
-            onMouseEnter={() => this.hoverHandler(true)}
-            onMouseLeave={() => this.hoverHandler(false)}
-          >
-            <Link to="/BBBGLS/contact">Contact</Link>
-          </li>
-        </ul>
+  return (
+    <div className="Navigation">
+      <div className="logo">
+        BBBGLS
       </div>
-    );
-  }
+      <ul>
+        <li><Link to="/BBBGLS/">About me</Link></li>
+        <li><Link to="/BBBGLS/abilities">Abilities</Link></li>
+        <li><Link to="/BBBGLS/work">Work</Link></li>
+        <li className={hoveringOnLastItem ? 'hover' : ''}>
+          <Link to="/BBBGLS/education">Education</Link>
+        </li>
+        <li
+          onMouseEnter={() => hoverHandler(true)}
+          onMouseLeave={() => hoverHandler(false)}
+        >
+          <Link to="/BBBGLS/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
