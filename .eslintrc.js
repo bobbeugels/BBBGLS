22b836f6e17b011ce69dee2f32fbf884bfd48957
bobbeugels <bob.beugels@gmail.com>
2019-02-07 21:40:02 +0100

    Updated react and eslint to support hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   "parser": "babel-eslint",
   "plugins": [
-    "react"
+    "react",
+    "react-hooks"
   ],
   "extends": "airbnb",
   "rules": {
-    "react/jsx-filename-extension": 0
+    "react/jsx-filename-extension": 0,
+    "react-hooks/rules-of-hooks": "error"
   }
 };
